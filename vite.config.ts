de41f7a2b4ab4864a1eeb7a97baacb7b29158458
Jaiden Suthar <Jaiden.suthar@titaniumit.com>
2026-05-23 22:50:10 -0500

    Revert "feat: add GitHub Actions deploy to GitHub Pages"
    This reverts commit 98f25d04b07a48075ce73f29c27563cff00f5ce0.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? '/vibeplans/' : '/',
   server: {
     proxy: {
       '/api': {
